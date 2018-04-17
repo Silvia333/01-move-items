f8b295f36ad2ec735ce5e691bdb0f0ff14df1580
Silvia de Lucas <silviadelucas@MacBook-Pro-de-Silvia.local>
2018-04-17 17:30:10 +0200

    commit js

---
+++ javascritps/main.js
@@ -0,0 +1,9 @@
+/*funcion para pasar un texto de un sitio a otro */
+var btn = document.getElementById("button");
+    btn.addEventListener("click", myFunction);
+
+    function myFunction(){
+        var txt1 = document.getElementById("txt1").innerHTML;
+        var txt2 = document.getElementById("txt2");
+        txt2.innerHTML=txt1;
+    }
